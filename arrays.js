48dae3d5f126705859aca13a11b01120c64d507f
Madeline Celis <madelinebcelis@gmail.com>
2018-07-15 23:16:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,8 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element){
-  var newArray = array.slice().unshift(element);
+  var newArray = array.slice();
+  newArray.unshift(element);
   return newArray;
 };
 
@@ -11,7 +12,8 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 };
 
 function addElementToEndOfArray(array, element){
-  var newArray = array.slice().push(element);
+  var newArray = array.slice();
+  newArray.push(element);
   return newArray;
 };
 
